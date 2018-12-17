08def30619c7956d534ad69e81980bbbb3780a16
smartkiddo <njenga05@gmail.com>
2018-12-17 12:34:16 +0300

    initial commit

---
+++ db/migrate/20181216130909_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :string
+      t.string :body
+      t.string :text
+
+      t.timestamps
+    end
+  end
+end
